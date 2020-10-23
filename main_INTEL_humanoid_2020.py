ef9d4f88ad9ebc04127d43577caf62c2b058ce8e
mateusouza39 <55259624+mateusouza39@users.noreply.github.com>
2020-10-23 17:09:53 -0300

    Add files via upload

--- main_INTEL_humanoid_2020.py
+++ main_INTEL_humanoid_2020.py
@@ -3,79 +3,101 @@
 Função principal do projeto Humanoide RoboIME 2020
 
 Tomada de decisão para transição entre os estados
-
 Feita para ser utilizada em Raspberry Pi
-
 Primeira versao
 
-Ainda não importa os arquivos que terão as funções Decisao_desvio(), enviarestado(),
-direcao_desvio nem Desvio()
+    OBSERVAÇÕES:
+    
+Ainda não importa os arquivos que terão as funções Decisao_desvio(), corrigirDirecao(),
+direcao_desvio() nem Desvio().
+
+O angulo de visão do sensor VL53L0X é de 25°
+
+Decisao_desvio()  --> funçao que determina para qual lado o robo tem que girar para desviar (SEPPE)
+direcao_desvio    --> funçao que mantem o robo girando até atingir a direçao correta pra realizar o desvio (CLARISSE)
+Desvio()          --> funçao que mantem o robo andando até ultrapassar o obstaculo desviado  (DOMINGOS)
+corrigirDirecao() --> funçao que mantem o robo girando até voltar para a direçao padrao da pista (DOMINGOS)
+
 
 @autores: Mateus Souza, Mateus Seppe, Matheus Domingos e Clarisse Rufino
 """
 
 ######################## BIBLIOTECAS #############################
 import time
-from math import degrees
+import serial
+import math
 import RTIMU
 import RPi.GPIO as GPIO
-import string
-import classes_main_INTEL_humanoide as Estado
+import classes_funcoes_INTEL_humanoide as Estado
 ######################## CONSTANTES #############################
 channel = 18                    #porta utilizada
+porta = "/dev/ttyS0"
+baudrate = 9600                 #deve igualar a da myrio
 GPIO.setmode(GPIO.BCM)          #configuraçoes das rasp
 GPIO.setup(channel, GPIO.OUT)
-intervalo = 1       #intervalo, em segundos, aceitavel entre as verificações de obstaculo e direção
-yaw_0 = 0         #direçao da pista de corrida
-yaw = 0         #direçao instantanea do robo
-limDyaw = 10    #limite aceitavel para diferença entre a direçao instantanea e a direçao correta
-dist = 1000       #menor distancia instantanea obtida pelo sensor
+
+intervalo = 0.6       #intervalo, em segundos, aceitavel entre as verificações de obstaculo e direção
+yaw_0 = 0         #direçao da pista de corrida (direçao padrao)
+Yaw = ([0])         #direçao instantanea de movimento do robo na pista, armazenada em formato lista
+limDyaw = 10    #limite aceitavel, em graus, para diferença entre a direçao instantanea e a direçao correta
+Dist = ([1000])       #menor distancia instantanea obtida pelo sensor, armazenada em formato lista
+Tmed = 5                #tempo medio, em segundos, pro robo andar 2cm, usado como argumento da funçao Desvio()
+Dmin = 46               #Distancia, em cm, a partir da qual o obstáculo é considerado "detectado" pra iniciar desvio
+
 ANDAR=0                 #
 GIRAR_ESQUERDA=1        #legenda dos estados
 GIRAR_DIREITA=2         #
 PARAR=3                 #
 
+ser = serial.Serial(porta,baudrate)       #configura a porta serial para fazer comunicação com a myrio
+
 ######################## Função main ############################
 Atual = Estado.estado(3) #iniciando no estado PARAR
-enviarestado()          #envia o estado para a placa myrio - diz qual arquivo de estado abrir e rodar nos motores
-
+ser.write(Atual.getName()) #envia o estado atual pra porta serial, pra ser lido depois pela myrio
 
 print("Programa rodando... pode ser interrompido usando CTRL+C")
 try:                    #utilizado pra possibilitar o programa ser interrompido
     while True:
-        print("Heavy task!")
+        print("Estado padrao")
         Atual = Estado.estado(0)        #Começar a andar
-        enviarestado()                 
-        dist = Distancia.getDistance()
-        yaw = Direcao.getYaw() - yaw_0
+        ser.write(Atual.getName())                 
+        Dist[0] = Distancia.getDistance()
+        Yaw[0] = Direcao.getYaw() - yaw_0
         
-        if (dist<=47):
+        if (Dist[0] <= Dmin):
             print ("obstaculo detectado")
             Atual = Estado.estado(3)    #parar para começar a desviar do obstaculo
-            enviarestado()
+            ser.write(Atual.getName())
+            
             Atual = Estado.estado(Decisao_desvio()) #obtem o lado para o qual deve girar e retorna 1 ou 2 (esquerda ou direita)
-            enviarestado()
-            Atual = Estado.estado(direcao_desvio)    #retorna 3 depois que obter a direcao de desvio apos girar suficiente
-            enviarestado()
+            ser.write(Atual.getName())
+            
+            Atual = Estado.estado(direcao_desvio(Yaw,Dist))    #retorna 3 depois que obter a direcao de desvio apos girar suficiente
+            ser.write(Atual.getName())
+            
             Atual = Estado.estado(0)                #volta a andar pra ultrapassar o obstaculo
-            enviarestado()
-            Atual = Estado.estado(Desvio())     #funçao retorna "3" dps que o robo terminar de ultrapassar
-            enviarestado()
+            ser.write(Atual.getName())
+            
+            Atual = Estado.estado(Desvio(Yaw,Dist))     #funçao retorna "3" dps que o robo terminar de ultrapassar, alem de alterar os valores
+            ser.write(Atual.getName())                  # de Yaw e Dist obtidos no fim do processo
             print("obstaculo ultrapassado")
             
-        if ((yaw-yaw_0) < (-limDyaw)):  
+        if ((Yaw[0]-yaw_0) < (-limDyaw)):  
             print("direçao incorreta - virado para DIREITA")
             Atual = Estado.estado(2)    #se a diferença for negativa, tem que girar para direita
-            enviarestado()
-            Atual = Estado.estado(corrigirDirecao()) #funçao retorna "3" dps que o robo corrigir a direçao
+            ser.write(Atual.getName())
+            
+            Atual = Estado.estado(corrigirDirecao(Yaw)) #funçao retorna "3" dps que o robo corrigir a direçao
+            ser.write(Atual.getName())
             print("direçao corrigida")
         
-        if ((yaw-yaw_0) > limDyaw):
+        if ((Yaw[0]-yaw_0) > limDyaw):
             print("direçao incorreta - vire para ESQUERDA")
             Atual = Estado.estado(1)    #se a diferença for positiva, tem que girar para esquerda
-            enviarestado()
-            Atual = Estado.estado(corrigirDirecao()) #funçao retorna "3" dps que o robo corrigir a direçao
-            enviarestado()
+            ser.write(Atual.getName())
+            
+            Atual = Estado.estado(corrigirDirecao(Yaw)) #funçao retorna "3" dps que o robo corrigir a direçao
+            ser.write(Atual.getName())
             print("direçao corrigida")
             
         time.sleep(intervalo)
@@ -85,6 +107,6 @@ except KeyboardInterrupt:
 
 #print("Continuando execução do código")
 Atual = Estado.estado(3)        #parar os motores por questao de segurança
-enviarestado()
+ser.write(Atual.getName())
 print(Atual)
     
