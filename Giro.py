f271d11845583b9c4c86ba982eb93316cda6c053
matheusdomingos007 <48094120+matheusdomingos007@users.noreply.github.com>
2020-10-27 13:18:36 -0300

    Update Giro.py

--- Giro.py
+++ Giro.py
@@ -7,7 +7,7 @@
 """
 Created on Oct 2020
     Funçoes
-    GetDistance
+    giro
 @author: Domingos
 """
 
@@ -18,20 +18,11 @@ import time
 
 
 ##"""Função"""
-#Função cronômetro
-def cronometro(t):
-    import time
-    start = time.time()
-    for x in range(t):
 
-        time.sleep(1)
-        if time.time() - start > t:
-            break
-    return True
 #A função giro fará o robo se alinhar a direção zero, então oq queremos é que o yaw volte ao ponto zero
 def giro(ang):#Deixe como argumento aqui o próprio Yaw
     while 1:
-        if abs(GetYaw())>1e-4
+        if abs(GetYaw())>1
             break
     return 3
     
