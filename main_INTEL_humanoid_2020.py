8fc80e89f11ffee456c31ba5c518156c73a1e4b2
mateusouza39 <55259624+mateusouza39@users.noreply.github.com>
2020-11-08 15:56:31 -0300

    Update main_INTEL_humanoid_2020.py

--- main_INTEL_humanoid_2020.py
+++ main_INTEL_humanoid_2020.py
@@ -27,8 +27,10 @@ import time
 import serial
 import math
 import RTIMU
+import get_yaw as Direcao
 import RPi.GPIO as GPIO
 import classes_funcoes_INTEL_humanoide as Estado
+import VL53L0X
 ######################## CONSTANTES #############################
 channel = 18                    #porta utilizada
 porta = "/dev/ttyS0"
@@ -36,11 +38,28 @@ baudrate = 9600                 #deve igualar a da myrio
 GPIO.setmode(GPIO.BCM)          #configuraçoes das rasp
 GPIO.setup(channel, GPIO.OUT)
 
-intervalo = 0.6       #intervalo, em segundos, aceitavel entre as verificações de obstaculo e direção
+tof = VL53L0X.VL53L0X()         # Criando o objeto associado ao sensor VL53L0X
+tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)  #configurando alcance e precisao do sensor
+
+SETTINGS_FILE = "/home/pi/IMU/RTEllipsoidFit/RTIMULib.ini"     #
+                                                                #iniciando o sensor giroscopio
+s = RTIMU.Settings(SETTINGS_FILE)                               #
+imu = RTIMU.RTIMU(s)                                            #
+
+imu.IMUInit()               #
+imu.setSlerpPower(0.02)     #   confirgurações giroscopio
+imu.setGyroEnable(True)     #
+imu.setAccelEnable(True)    #
+imu.setCompassEnable(True)  #
+
+poll_interval = imu.IMUGetPollInterval()
+
+
+intervalo = 0.1       #intervalo, em segundos, aceitavel entre as verificações de obstaculo e direção
 yaw_0 = 0         #direçao da pista de corrida (direçao padrao)
 Yaw = ([0])         #direçao instantanea de movimento do robo na pista, armazenada em formato lista
 limDyaw = 10    #limite aceitavel, em graus, para diferença entre a direçao instantanea e a direçao correta
-Dist = ([1000])       #menor distancia instantanea obtida pelo sensor, armazenada em formato lista
+Dist = ([100])       #menor distancia instantanea obtida pelo sensor, armazenada em formato lista
 Tmed = 5                #tempo medio, em segundos, pro robo andar 2cm, usado como argumento da funçao Desvio()
 Dmin = 46               #Distancia, em cm, a partir da qual o obstáculo é considerado "detectado" pra iniciar desvio
 
@@ -51,6 +70,7 @@ PARAR=3                 #
 
 ser = serial.Serial(porta,baudrate)       #configura a porta serial para fazer comunicação com a myrio
 
+
 ######################## Função main ############################
 Atual = Estado.estado(3) #iniciando no estado PARAR
 ser.write(Atual.getName()) #envia o estado atual pra porta serial, pra ser lido depois pela myrio
@@ -60,47 +80,55 @@ try:                    #utilizado pra possibilitar o programa ser interrompido
     while True:
         print("Estado padrao")
         Atual = Estado.estado(0)        #Começar a andar
-        ser.write(Atual.getName())                 
-        Dist[0] = Distancia.getDistance()
-        Yaw[0] = Direcao.getYaw() - yaw_0
+        ser.write(Atual.getName())    
+             
+        Dist[0] = tof.get_distance()/10 #distancia obtida pelo sensor em centimetros
+        Yaw[0] = Direcao.get_yaw(intervalo)
         
         if (Dist[0] <= Dmin):
             print ("obstaculo detectado")
             Atual = Estado.estado(3)    #parar para começar a desviar do obstaculo
             ser.write(Atual.getName())
             
-            Atual = Estado.estado(Decisao_desvio()) #obtem o lado para o qual deve girar e retorna 1 ou 2 (esquerda ou direita)
+            time.sleep(3) #apenas pra teste
+            
+            Atual = Estado.estado(Estado.Decisao_desvio()) #obtem o lado para o qual deve girar e retorna 1 ou 2 (esquerda ou direita)
             ser.write(Atual.getName())
             
-            Atual = Estado.estado(direcao_desvio(Yaw,Dist))    #retorna 3 depois que obter a direcao de desvio apos girar suficiente
+            time.sleep(1) #apenas pra teste
+            
+            Atual = Estado.estado(Estado.direcao_desvio(Yaw,Dist))    #retorna 3 depois que obter a direcao de desvio apos girar suficiente
             ser.write(Atual.getName())
+            
+            time.sleep(1) #apenas pra teste
             
             Atual = Estado.estado(0)                #volta a andar pra ultrapassar o obstaculo
             ser.write(Atual.getName())
             
-            Atual = Estado.estado(Desvio(Yaw,Dist))     #funçao retorna "3" dps que o robo terminar de ultrapassar, alem de alterar os valores
+            time.sleep(1) #apenas pra teste
+            
+            Atual = Estado.estado(Estado.Walk_Detour(Dist,Yaw))     #funçao retorna "3" dps que o robo terminar de ultrapassar, alem de alterar os valores
             ser.write(Atual.getName())                  # de Yaw e Dist obtidos no fim do processo
             print("obstaculo ultrapassado")
             
-        if ((Yaw[0]-yaw_0) < (-limDyaw)):  
+        if ((Yaw[0]) < (-limDyaw)):  
             print("direçao incorreta - virado para DIREITA")
             Atual = Estado.estado(2)    #se a diferença for negativa, tem que girar para direita
             ser.write(Atual.getName())
             
-            Atual = Estado.estado(corrigirDirecao(Yaw)) #funçao retorna "3" dps que o robo corrigir a direçao
+            Atual = Estado.estado(Estado.giro()) #funçao retorna "3" dps que o robo corrigir a direçao
             ser.write(Atual.getName())
             print("direçao corrigida")
         
-        if ((Yaw[0]-yaw_0) > limDyaw):
+        if ((Yaw[0]) > limDyaw):
             print("direçao incorreta - vire para ESQUERDA")
             Atual = Estado.estado(1)    #se a diferença for positiva, tem que girar para esquerda
             ser.write(Atual.getName())
             
-            Atual = Estado.estado(corrigirDirecao(Yaw)) #funçao retorna "3" dps que o robo corrigir a direçao
+            Atual = Estado.estado(Estado.giro()) #funçao retorna "3" dps que o robo corrigir a direçao
             ser.write(Atual.getName())
             print("direçao corrigida")
             
-        time.sleep(intervalo)
         
 except KeyboardInterrupt:
     print(" CTRL+C detectado. O loop foi interrompido.")
