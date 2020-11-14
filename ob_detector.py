e5e3abcb030a84fc784f7787db802f61f4eb831b
mateusseppe <54176903+mateusseppe@users.noreply.github.com>
2020-11-14 03:33:03 -0300

    Rename obj_detector.py to ob_detector.py

--- ob_detector.py
+++ ob_detector.py
@@ -2,45 +2,59 @@ import cv2
 import numpy as np
 
 
-def xy(image):
-    img = cv2.imread(image)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #cv2.imshow('HSV', img_hsv)
-    kernel = np.ones((5, 5), np.uint8)
-    rangomax = np.array([20, 255, 255])
-    rangomin = np.array([5, 125, 90])
+def xy():  # Argumento imagem #Retorno o ponto médio da linha mais baixa(xmed,ymed)
+    image = cv2.imread('image_3\i2_2.jpg')
 
-    mask = cv2.inRange(img_hsv, rangomin, rangomax)
-    #cv2.imshow('mask', mask)
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    #cv2.imshow('opening', opening)
-    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-    #cv2.imshow('closing', closing)
-    closing2 = cv2.morphologyEx(closing, cv2.MORPH_CLOSE, kernel)
-    #cv2.imshow('closing2', closing2)
-    edges = cv2.Canny(closing2, 200, 200)
+    # HLS filtro
+    lower = np.array([5, 0, 0])
+    upper = np.array([25, 255, 255])
 
-    contours, hierarchy = cv2.findContours(
-        edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours_poly = [None]*len(contours)
-    boundRect = [None]*len(contours)
-    for i, c in enumerate(contours):
-        contours_poly[i] = cv2.approxPolyDP(c, 3, True)
-        boundRect[i] = cv2.boundingRect(contours_poly[i])
-    drawing = np.zeros((edges.shape[0], edges.shape[1], 3), dtype=np.uint8)
-    for i in range(len(contours)):
-        color = (0, 255, 0)
-        if boundRect[i][2] > 250:
-            #cv2.drawContours(img, contours_poly, i, color)
-            # cv2.rectangle(img, (int(boundRect[i][0]), int(boundRect[i][1])),
-            # (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
-            #cv2.rectangle(img, (0, 0), (20, 20), color, 2)
-            x = int(boundRect[i][0]+boundRect[i][2]/2)
-            y = int(boundRect[i][1]+boundRect[i][3]/2)
-            print(x, y)
-            return x, y
-        else:
-            pass
+    # A placa nos envia a imagem rotacionada, vamos então aqui rotacionar a imagem usando o centro como origem:
+    (alt, lar) = image.shape[:2]  # captura altura e largura
+    centro = (lar // 2, alt // 2)  # acha o centro
 
+    M = cv2.getRotationMatrix2D(centro, 180, 1.0)  # Gerar matriz de rotação
+    image = cv2.warpAffine(image, M, (lar, alt))  # Comando para rotacionar
 
-xy('images/imagem5.jpeg')
+    # image1 = cv2.blur(image, (7, 7))  # Suavizar a imagem utilizando blur
+
+    hls = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)  # Converter o COLORSPACE
+    # Criação da mascara para objetos laranja
+    orangemask = cv2.inRange(hls, lower, upper)
+
+    # Usamos o canny para pegar os contornos
+    edges = cv2.Canny(orangemask, 100, 300, apertureSize=3)
+
+    # cv2.imwrite('edges'+str(n+1)+".jpeg",edges)
+    # cv2.imwrite('mask'+str(n+1)+".jpeg",orangemask)
+
+    minLineLength = 90  # Parametro da HoughLines
+
+    # Utlizar HoughLinesP para retornar x1 y1 x2 y2
+    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=100,
+                            lines=np.array([]), minLineLength=minLineLength, maxLineGap=200)
+
+    # a = número de linhas (matriz)
+    # b = número de linhas da lista
+    # c = 4, pois é a DIM
+
+    a, b, c = lines.shape
+
+    # A padronização dos eixos no OpenCV é o eixo y para baixo, portanto ymax=-1 da sempre valores maiores que os medidos
+    maxy = -1
+
+    linha_objeto = []
+    for i in range(a):
+        if lines[i][0][1] > maxy or lines[i][0][3] > maxy:
+            maxy = max(lines[i][0][1], lines[i][0][3])
+            indice = i
+
+    linha_objeto = lines[indice][0]
+
+    if linha_objeto != []:
+        medy = (linha_objeto[1]+linha_objeto[3])/2  # Ponto médio em y
+        medx = (linha_objeto[0]+linha_objeto[2])/2  # Ponto médio em x
+    else:
+        medy = 0
+        medx = 0
+    return medx, medy
