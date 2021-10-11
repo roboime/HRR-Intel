fea67ce86bcebbe7618cbff58564667f9bdd3fb9
ebert-hvm <ebert.hv.melo@gmail.com>
2021-10-11 16:31:56 -0300

    ranges_preto em vez de branco

--- 2021/visao.py
+++ 2021/visao.py
@@ -206,7 +206,7 @@ def bordas_laterais_v1(objeto_imagem):
  e maximo para considerar como borda. A segunda eh o topo_da_pista, que seleciona apenas as retas que estao abaixo do topo da pista para evitar ruidos. A terceira
   eh o meio_da_pista que divide as retas da borda da esquerda e da borda da direita."""
 def bordas_laterais_v2(objeto_imagem):
-    mask = objeto_imagem.mask("ranges_branco.txt")
+    mask = objeto_imagem.mask("ranges_preto.txt")
     reconhecer_pista(mask, objeto_imagem)
     img = objeto_imagem.img
     edges = cv2.Canny(mask, 50, 150, apertureSize=3)
