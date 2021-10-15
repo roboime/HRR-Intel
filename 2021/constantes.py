819f63d663061387a9c9b94857595f8bd13d3b64
pedrobpn <pbpn2000@hotmail.com>
2021-10-14 21:02:28 -0300

    update 10_fotos.py

--- 2021/constantes.py
+++ 2021/constantes.py
@@ -59,6 +59,18 @@ velocidade_angular[int(GIRAR_DIREITA)] = velocidade_ang_em_graus[int(GIRAR_DIREI
 
 # CONSTANTES DE VISAO
 
+NAO_HA_RETA = 0
+HA_DUAS_RETAS = 1
+SO_ESQUERDA = 2
+SO_DIREITA = 3
+
+X1 = 0
+Y1 = 1
+X2 = 2
+Y2 = 3
+
+RANGE_INCLINACAO = 80 #Em graus
+
 constraste_da_camera = 90
 tolerancia_central = 15
 tolerancia_para_frente = 60
--- 2021/tests/10_fotos.py
+++ 2021/tests/10_fotos.py
@@ -9,11 +9,11 @@ camera.start_preview()
 sleep(2)
 #tempo = int(input("Entre com o tempo entre as fotos"))
 #numero_fotos = int(input("Entre com o numero de fotos"))
-tempo = 1
-numero_fotos = 21
+tempo = 5
+numero_fotos = 10
 for i in range(numero_fotos):
     diretorio_com_nome = "/home/pi/HRR-Intel/2021/tests/imagem_teste_contraste_" + str(i) + ".jpg"
-    camera.contrast = 5*i
+    camera.contrast = 90
     camera.capture(diretorio_com_nome)
     print("tirei a foto, proxima foto em: ")
     for j in range(tempo):
