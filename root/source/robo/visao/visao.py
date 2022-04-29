011f4cb06a7132936f62e0677a4264df02287e77
Caruso <jpfcaruso@gmail.com>
2022-04-29 18:43:58 -0300

    Alterações nas classes visão e camera

--- root/source/robo/visao/visao.py
+++ root/source/robo/visao/visao.py
@@ -1,51 +1,40 @@
 import cv2
 
-class Imagem():
+class Visao():
     ''' 
 Classe relacionada a imagem obtida pela camera. Ao ser chamada, inverte a imagem e salva constantes relacionadas a imagem, como altura, largura e centro.
 Possui o metodo mask, que retorna a mascara da imagem, passando o arquivo onde esta salvo os ranges da cor.
 '''
-    def __init__(self, path):
-        """esse eh o init"""
-        print("Entrando no _init_ do Classe_imagem()")
-        img = cv2.imread(path)
-        #img = np.array(img)
-
-        img = cv2.rotate(img, cv2.ROTATE_180)
-        cv2.imwrite("/home/pi/Pictures/imagem_girada.jpg", img)
-
-        img.astype(np.uint8)
-
-        (self.altura, self.largura) = img.shape[:2] 
-        self.centro = ( (self.largura)/2, (self.altura)/2 )
-
-
-        #M = cv2.getRotationMatrix2D(self.centro, 180, 1)
-
-        print("Altura: {}  Largura: {}".format(self.altura,self.largura))
-
-        #img = cv2.warpAffine(img, M, (self.largura, self.altura))
+    def __init__(self):
+        self.imagem = Imagem()
+    def checar_alinhamento_pista(self):
+        left, right, case = imagem.bordas_laterais()
+        horizontal = [0, imagem.topo_da_pista, imagem.largura, imagem.topo_da_pista]
+        x1 = 0
+        x2 = imagem.largura
         
+        if case ==NAO_HA_RETA:
+            return ANDAR
+        elif case == HA_DUAS_RETAS:
+            x1, _ = imagem.interscetion(horizontal, left)
+            x2, _ = imagem.interscetion(horizontal, right)
+        elif case == SO_ESQUERDA:
+            x1, _ = imagem.interscetion(horizontal, left)
+        elif case == SO_DIREITA:
+            x2, _ = imagem.interscetion(horizontal, right)  
 
-        print("SAIMO DO WARPAFFINE")
-        self.img = img
-        self.topo_da_pista = int(0.4*self.altura) #coordenada y do topo da pista
-        self.meio_da_pista = 0 # coordenada x do meio da pista
-        self.largura_pista = 0 # largura do final da pista na imagem
-        self.mult_largura_pista = 0.7 #ate quanto da metade da largura da pista ainda eh atravessavel pelo robo
-        print("Saindo do _init_ do Classe_imagem()")
-
-    def mask(self, ranges_file_path):
-        hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV) # converte a cor para hsv
-        with open(ranges_file_path, "r") as f:
-            lines = f.readlines()
-            range = lines[0].split(",")
-            lower = np.array([int(range[0]),int(range[1]),int(range[2])])  #range de cores em hsv para reconhecer as bordas
-            upper = np.array([int(range[3]),int(range[4]),int(range[5])])
-        mask = cv2.inRange(hsv, lower, upper)
-        kernel = np.ones((5,5), np.uint8) 
-        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=2)
-        return mask
-
-if __name__ == "__main__":
-    help(Imagem)
+        largura_pista = abs(x2 - x1)
+        mult_largura_pista = 0.3
+        delta_x = (x1+x2)//2-imagem.largura//2
+        #imagem.img = cv2.line(imagem.img, (imagem.largura//2, 0), (imagem.largura//2, imagem.altura), (255, 0, 0), 2)
+        #imagem.img = cv2.line(imagem.img, (int(imagem.largura_pista//2*imagem.mult_largura_pista)+(x1+x2)//2, 0), (int(imagem.largura_pista//2*imagem.mult_largura_pista)+(x1+x2)//2, imagem.altura), (127, 127, 0), 2)
+        #imagem.img = cv2.line(imagem.img, (-int(imagem.largura_pista//2*imagem.mult_largura_pista)+(x1+x2)//2, 0), (-int(imagem.largura_pista//2*imagem.mult_largura_pista)+(x1+x2)//2, imagem.altura), (127, 127, 0), 2)
+        #imagem.img = cv2.circle(imagem.img, (x1, imagem.topo_da_pista), radius=10, color=(0, 255, 255), thickness=-1)
+        #imagem.img = cv2.circle(imagem.img, (x2, imagem.topo_da_pista), radius=10, color=(0, 255, 255), thickness=-1)
+        #imagem.img = cv2.circle(imagem.img, ((x1+x2)//2, imagem.topo_da_pista), radius=10, color=(0, 0, 255), thickness=-1)
+        if largura_pista//2*mult_largura_pista > abs(delta_x):
+            return ANDAR
+        elif delta_x > 0:
+            return GIRAR_DIREITA
+        else:
+            return GIRAR_ESQUERDA 
