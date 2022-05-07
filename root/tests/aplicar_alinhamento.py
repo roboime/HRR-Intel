7cece80e2501d5ac87d3eff5101846428dfd0362
ebert-hvm <ebert.hv.melo@gmail.com>
2022-05-07 13:14:52 -0300

    teste da aplicar_alinhamento

--- root/tests/aplicar_alinhamento.py
+++ root/tests/aplicar_alinhamento.py
@@ -7,7 +7,7 @@ from source import robo
 
 path = "../data/images/input_imgs/"
 
-IMAGES = [robo.Visao.from_path(join(path, f)) for f in listdir(join(path))]
+VISAO = [robo.Visao.from_path(join(path, f)) for f in listdir(join(path))]
 
 font                   = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10,30)
@@ -17,7 +17,7 @@ lineType               = 2
 
 if __name__ == "__main__":
     cnt=1
-    for OBJ in IMAGES:
+    for OBJ in VISAO:
 
         frame = OBJ.img
         width = OBJ.largura
@@ -26,10 +26,12 @@ if __name__ == "__main__":
         image = np.zeros(frame.shape, np.uint8)
         smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
         frame = cv2.rotate(frame, cv2.ROTATE_180)
-        obj = Classe_imagem(cv2.resize(frame, (0, 0), fx=0.5, fy=0.5))
-        mask_img = obj.mask('./ranges_branco.txt')
+        obj = robo.Visao(cv2.resize(frame, (0, 0), fx=0.5, fy=0.5), None)
+        mask_img = obj.mask("../data/filtros_de_cor/ranges_preto.txt")
         mask_img2 = cv2.Canny(mask_img, 50, 150, apertureSize=3)
-        _ = checar_alinhamento_pista_v2(obj)
+        _ = obj.decisao_alinhamento()
+        obj.desenhar_bordas()
+        obj.desenhar_alinhamento()
         for i in range(mask_img.shape[0]):
             for j in range(mask_img.shape[1]):
                 image[i][j][:3] = mask_img[i][j]
@@ -41,6 +43,6 @@ if __name__ == "__main__":
         image[height//2:, :width//2] = smaller_frame
         image[height//2:, width//2:] = obj.img
         
-        cv2.imwrite("./tests/imagens/output_imgs/"+str(cnt)+".png", image)
+        cv2.imwrite("../data/images/output_imgs/teste_alinhamento/"+str(cnt)+".png", image)
         cnt+=1
   
