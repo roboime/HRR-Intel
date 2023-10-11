d8f8096f706ce1ed7ef6fa54ab56baaa92efe74f
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-10-11 13:57:20 -0300

    modificacao corrida.py

--- hrr/robo.py
+++ hrr/robo.py
@@ -4,7 +4,7 @@ from desvio import DesvioObstaculo
 from estado import Estado
 from serial_com import SerialMyrio
 from imu6050 import Imu6050 
-from visao import Classe_imagem
+from visao import Classe_imagem, checar_alinhamento_pista_v2
 from sensor_distancia import SensorDistancia
 from camera import RaspCamera
 from time import sleep, time
@@ -42,14 +42,14 @@ class Robo:
                 print("hora de alinhar")
                 self.estado.trocar_estado("PARAR") ##tirar esse tempo ja que as pausas devem estar embutidas no tirar foto e na troca de estados
                 sleep(c.tempo_para_parar)
-                self.estado.trocar_estado(self.visao.checar_alinhamento_pista_v2(self.visao))  # Frente, GIRAR_ESQUERDA ou GIRAR_DIREITA
+                self.estado.trocar_estado(checar_alinhamento_pista_v2(self.visao))  # Frente, GIRAR_ESQUERDA ou GIRAR_DIREITA
                 numero_de_giradas = 1
                 while self.estado.obter_estado_atual() == "GIRAR_DIREITA" or self.estado.obter_estado_atual() == "GIRAR_ESQUERDA":
                     print("desalinhado com a pista, iniciando a ",numero_de_giradas, "a girada")
                     sleep(self.tempo_do_passo[self.estado.obter_estado_atual()])
                     self.estado.trocar_estado("PARAR")
-                    sleep(self.tempo_do_passo["PARAR"])
-                    self.estado.trocar_estado(self.visao.checar_alinhamento_pista_v2(self.visao))
+                    sleep(c.tempo_do_passo["PARAR"])
+                    self.estado.trocar_estado(checar_alinhamento_pista_v2(self.visao))
                     numero_de_giradas+=1
                 print("direcao corrigida")
                 numero_de_giradas = 1
